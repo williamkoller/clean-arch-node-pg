b0bdb34d47c2d40c720026ba2b62020294f959f5
William Koller <williamkoller404@gmail.com>
2023-05-14 22:37:34 -0300

    fix: remove code disuse

--- src/infra/database/typeorm/repositories/role-typeorm-repository.ts
+++ src/infra/database/typeorm/repositories/role-typeorm-repository.ts
@@ -1,7 +1,6 @@
 import { AddRoleRepository } from '@/data/protocols/db/role/add-role-repository';
 import { AppDataSource } from '../config/data-source';
 import { RoleEntity } from '../entities/role.entity';
-import { RoleEntityInMemory } from '../../inmemory/entities/role-entity-in-memory.ts';
 import { AddRoleParams } from '@/domain/usecases/role/add-role';
 import { LoadRoleByNameRepository } from '@/data/protocols/db/role/load-role-by-name-repository';
 
